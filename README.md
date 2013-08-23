34a4c778b49a4bd3b1ef1625e6900e6d2c23715c
Julio Cesar Rodriguez Dominguez <jurasec@gmail.com>
2013-08-22 21:03:48 -0500

    se agrega animación para reacomodar la cartas en cada turno. Se documenta un poco el código

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ hearts
 ======
 
 Hearts game
+
+http://www.1stwebdesigner.com/css/how-to-use-css3-orientation-media-queries/
+http://springload.responsinator.com/?url=localhost%2Fhearts%2F
+http://mattkersley.com/responsive/
+http://www.responsivewebcss.com/
+http://www.youtube.com/watch?v=LZ6J08yqGcQ&list=LL24iQ2_KinqBYB4WqFLKKuQ
+http://craftyjs.com/                       - Craftyjs
+http://inspiretrends.com/best-html5-development-tools/ - 20 Best HTML5 Development Tools to Save your Time
+http://indiegamr.com/retro-style-plattform-runner-game-for-mobile-with-easeljs-part-1/  - EaselJS
+http://hub.tutsplus.com/tutorials/getting-started-with-easeljs-a-flash-like-interface-for-the-html5-canvas--active-10382 - EaselJS
+http://www.createjs.com/#!/CreateJS        - CreateJS
+http://www.gamedev5.com/2013/07/tutorial-3-dragon-of-bosnia-classes-in.html   - CreateJS
+http://www.limejs.com/   - Body como pantalla completa.
+http://www.youtube.com/watch?v=4uOscmebR78 - LimeJS
+http://jawsjs.com/
+
+== Spring ==
+
+http://static.springsource.org/spring/docs/3.0.x/spring-framework-reference/html/jdbc.html#jdbc-StoredProcedure
+
+Media queries
+
+(window.matchMedia('(min-device-width : 320px) and (max-device-width : 1440px) and (orientation:landscape)').matches)
--- js/hearts.js
+++ js/hearts.js
@@ -4,15 +4,29 @@
 var widthDevice = window.innerWidth;
 var heightDevice = window.innerHeight;
 
+// ancho real de la carta (sin escalar)
 var realCardWidth = 200;
+// alto real de la carta (sin escalar)
 var realCardHeight = 250;
-var scale = ((widthDevice/15) * 1) / realCardWidth;
+// proporción de escalamiento
+var scale = ( ( widthDevice / 15 ) * 1 ) / realCardWidth;
+// ancho de la carta escalada
 var widthCard = realCardWidth * scale;
+// alto de la carta escalada
 var heightCard = realCardHeight * scale;
+// valor para ajustar algunos desplazamientos de las cartas
 var offset = 0;
-var spaceToolBar=215;
+// valor temporal para restar el ancho de la barra de busqueda
+var spaceToolBar= 215;
+// valor del eje Y para saber donde debe dibujarse las cartas del jugador
 var cardOffsetY = heightDevice - heightCard - spaceToolBar;
-var cardPosition = 1;
+// contardo de turnos
+var currentTurn = 1;
+
+var jsonCards = 
+[{"player1":["C1","C2","C9","C12","C13","D6","D12","D13","S10","S11","H2","H11","H12"]},{"player2":["C5","C8","D1","D3","D7","D8","D10","S3","S12","H3","H5","H7","H13"]},{"player3":["C6","D2","D4","D5","D11","S2","S5","S8","S9","S13","H1","H4","H9"]},{"player4":["C3","C4","C7","C10","C11","D9","S1","S4","S6","S7","H6","H8","H10"]}];
+// var jsonDroppedCards = { turno1:"", turno2:"", turno3:"", turno4:"", turno5:"", turno6:"", turno7:"", turno8:"", turno9:"", turno10:"", turno11:"", turno12:"", turno13:""};
+
 
 console.log("Card width: "+widthCard);
 console.log("Car height: "+heightCard);
@@ -27,6 +41,7 @@ console.log("Car height: "+heightCard);
     
 **/
 
+// objeto principal sobre el que se dibujan el resto de objetos
 var layer = new collie.Layer({
     width: widthDevice,
     height: heightDevice
@@ -91,17 +106,17 @@ collie.ImageManager.add({
     // spriteClubs: "images/spritesClubs.png"
 }, function(){ console.log("Imagen cargada 1.");});
 
+// gray box
 var box = new collie.DisplayObject({
     width : widthDevice * 0.3,
     height : heightDevice * 0.3,
-    x : (widthDevice/2) - ((widthDevice * 0.3)/2),
-    y : (heightDevice/2) - ((heightDevice * 0.3)/2),
+    x : ( widthDevice / 2 ) - ( ( widthDevice * 0.3 ) / 2 ),
+    y : ( heightDevice / 2 ) - ( ( heightDevice * 0.3 ) / 2 ) - 40,
     backgroundColor : 'gray'
 }).addTo(layer)
 
-var jsonCards = 
-[{"player1":["C1","C2","C9","C12","C13","D6","D12","D13","S10","S11","H2","H11","H12"]},{"player2":["C5","C8","D1","D3","D7","D8","D10","S3","S12","H3","H5","H7","H13"]},{"player3":["C6","D2","D4","D5","D11","S2","S5","S8","S9","S13","H1","H4","H9"]},{"player4":["C3","C4","C7","C10","C11","D9","S1","S4","S6","S7","H6","H8","H10"]}];
-// console.log(cards);
+/* ****************************************************************************************************************************** */
+// representa las cartas del jugador
 var Deck = function(){
     this.cards = new Array();
 }
@@ -109,162 +124,247 @@ var Deck = function(){
 Deck.prototype.addCard = function( card ){
     this.cards.push( card );
 }
+
+Deck.prototype.getCards = function( ){
+    return this.cards;
+}
+/* ****************************************************************************************************************************** */
+
 var playerDeck = new Deck();
-var oSelectedDisplayObject = null;
-var htStartPosition = {};
-var htSelectedDisplayObjectPosition = {};
+// var oSelectedDisplayObject = null;
+// var htStartPosition = {};
+// var htSelectedDisplayObjectPosition = {};
 
 
 function drawDeck(){
-    var position=0;
-    $.each(jsonCards[0].player1, function( key , cardValue ){
-        console.log( key +" -> " + cardValue); 
+    var cardPosition = 1;
+    var leftSideValue = -1;
+    var rightSideValue = -1;
+    var cardsPlayer = jsonCards[ 0 ].player1;
+    var cardValue='';
+    
+    // $.each(jsonCards[0].player1, function( key , cardValue ){
+    for(var i = 0; i < cardsPlayer.length; i++){
 
+        cardValue = cardsPlayer[ i ];
+        /**
+         * se va comprando si existe una carta a la izquierda o la derecha, de existir se asocia, para tener estos
+         * valores en cualquier momento.
+         **/
+        if(cardPosition == 0){
+            leftSideValue = -1;
+            rightSideValue = cardsPlayer[ i + 1 ]
+        }
+        else if( cardPosition == 13){
+            leftSideValue = cardsPlayer[ i - 1 ];
+            rightSideValue = -1;
+        }
+        else{
+            leftSideValue = cardsPlayer[ i - 1 ];
+            rightSideValue = cardsPlayer[ i + 1 ]
+        }
+
+        // se crea el objeto que representa una carta según sea cardValue.
         var tmpCard = new collie.DisplayObject({
-            x: (widthCard*(cardPosition+(position++)))+offset,
+            x: ( widthCard * ( cardPosition ) ) + offset,
             y: cardOffsetY,
-            scaleX:scale,
-            scaleY:scale,
+            scaleX: scale,
+            scaleY: scale,
             originX: 'left',
             originY: 'bottom',
             useEvent : true,
-            backgroundImage: cardValue
+            backgroundImage: cardValue,
+            cPosition: cardPosition - 1,
+            leftSide: leftSideValue,
+            rightSide: rightSideValue,
+            dropped: false
             // velocityRotate: 50
-        }).addTo(layer);
+        }).addTo( layer );
 
-        // Animations
-        var currentY = tmpCard.get("y");
+        cardPosition++;
+
+        // se configuran las animaciones y la respuesta al evento click/touch
+        var currentY = tmpCard.get( 'y' );
         tmpCard.attach({
-            mousedown : function (oEvent) {
-                // console.log("[displayobject] mousedown");
-                // if (oEvent.displayObject) {
-                //     console.log("over card...");
-                //     oSelectedDisplayObject = oEvent.displayObject;
-                //     console.log(oSelectedDisplayObject.get("scaleX"));
-                //     oEvent.displayObject.set({
-                //         scaleX : oEvent.displayObject.get("scaleX") * 1.2,
-                //         scaleY : oEvent.displayObject.get("scaleY") * 1.2
-                //     });
-                //     console.log(oSelectedDisplayObject.get("scaleX"));
-                //     // oSelectedDisplayObject = oEvent.displayobjectplayObject;
-                //     htStartPosition = {
-                //         x : oEvent.x,
-                //         y : oEvent.y 
-                //     };
-                //     htSelectedDisplayObjectPosition = {
-                //         x : oSelectedDisplayObject.get("x"),
-                //         y : oSelectedDisplayObject.get("y")
-                //     };
-                // }
-            },
-            mouseup : function (oEvent) {
-                // if (oSelectedDisplayObject !== null) {
-                //     console.log("regresando valores");
-                //     oEvent.displayObject.set({
-                //         scaleX : oSelectedDisplayObject.get("scaleX") / 1.2,
-                //         scaleY : oSelectedDisplayObject.get("scaleY") / 1.2
-                //     });
-                //     oSelectedDisplayObject = null;
-                //     htSelectedDisplayObjectPosition = htStartPosition = {};
-                    
-                //     layer.removeChild(oEvent.displayObject);
-                //     layer.addChild(oEvent.displayObject);
-                // }
-            },
-            mousemove : function (oEvent) {
-                console.log("moving...");
-                console.log(oEvent);
-                if (oEvent.displayObject) {
-                    console.log("over card...");
-                    oEvent.displayObject.set({
-                        scaleX : oEvent.displayObject.get("scale") * 1.2,
-                        scaleY : oEvent.displayObject.get("scale") * 1.2
-                    });
-                    oSelectedDisplayObject = oEvent.displayObject;
-                    htStartPosition = {
-                        x : oEvent.x,
-                        y : oEvent.y 
-                    };
-                    htSelectedDisplayObjectPosition = {
-                        x : oSelectedDisplayObject.get("x"),
-                        y : oSelectedDisplayObject.get("y")
-                    };
-                }
-            },         
-            click: function (oEvent) {
-                // Perform specific motion
-                var nScale = Math.random()*0.4+0.6; 
-                // console.log(oEvent.displayObject);           
+            click: function ( oEvent ) {        
                 collie.Timer.queue().
-                    transition( tmpCard, 200, {
-                        to: [oEvent.displayObject.get("y") - 140, (widthDevice/2) - ((widthCard)/2)],             // sinusoidal
+                    transition( oEvent.displayObject, 200, {                        
+                        to: [ oEvent.displayObject.get( 'y' ) - 170, ( widthDevice / 2 ) - ( ( widthCard ) / 2 ) ],  // sinusoidal
                         effect: collie.Effect.easeOutBack, // sinusoidal easeIn, //collie.Effect.cubicBezier(0.16, 0.79, 0.92, 0.27),
-                        set: ["y","x"]
+                        set: [ 'y', 'x' ]
                     });
-                        // .
-                        // transition(cQ, 400, {
-                        //     to: currentX,
-                        //     effect: collie.Effect.easeIn,
-                        //     set: "x"
-                        // });
-                // oEvent.displayObject.set({
-                //     angle : 45, //Math.random() * 360 | 0, 
-                //     scaleY : nScale,
-                //     scale : nScale
-                // });
+                
+                // se envian las cartas siempre al frente de todas.
+                oEvent.displayObject.set({
+                        zIndex: 1
+                    }
+                );
+                // indicamos que la carta ha sido jugada.
+                oEvent.displayObject.set({
+                    dropped: true
+                });
+
+                // jsonDroppedCards["turno"+currentTurn]= oEvent.displayObject.get('backgroundImage');
+                currentTurn++;
+                reAlign(oEvent.displayObject);                        
             }                        
         });
 
-        playerDeck.addCard(tmpCard);
-    });
+        playerDeck.addCard( tmpCard );
+    // });
+    }
 }
 
+function reAlign( cardDropped ){
+    var deck = playerDeck.getCards();
+    var posXRef = cardDropped.get( 'x' );
+    // for(var i = 0; i < playerDeck.getCards().length; i++){
+        // console.log("carta ["+(i+1)+"]: "+ deck[i].get("backgroundImage"));
+    // }
+    console.log('leftSide: '+cardDropped.get( 'leftSide' )+', rightSide: '+cardDropped.get( 'rightSide' ));
+    if( cardDropped.get( 'leftSide' ) == -1 || cardDropped.get( 'rightSide' ) == -1) {
+        console.log('no se hace nada.');
+        return;
+    }
+    else{
+        console.log('dropped: ['+cardDropped.get('dropped')+'] se recorren las cartas de la izquierda a derecha, inicia con: '+( cardDropped.get('cPosition') - 1) );
+        // recorre las cartas que estan a la izquierda de la carta elegida
+        for( var i = ( cardDropped.get( 'cPosition' ) - 1 ) ; i >= 0 ; i--){
+            if( !deck[ i ].get( 'dropped' ) ){
+                console.log('moviendo carta en posicion: '+ i);
+                console.log('moviendo: '+deck[ i ].get( 'backgroundImage' ) );
+                collie.Timer.queue().
+                        transition( deck[ i ], 200, {
+                            to: deck[ i ].get( 'x' ) + ( widthCard / 2 ),
+                            effect: collie.Effect.easeOutBack, // sinusoidal easeIn, //collie.Effect.cubicBezier(0.16, 0.79, 0.92, 0.27),
+                            set: 'x'
+                        });
+            }
+        }
 
+        console.log('se recorren las cartas de la derecha a la izquierda, inicia con: '+( cardDropped.get('cPosition') + 1 ));
+        // recorre las cartas que estan a la derecha de la carta elegida
+        for( var i = ( cardDropped.get( 'cPosition' ) + 1 ) ; i <=   12 ; i++){
+            if( !deck[ i ].get( 'dropped' ) ){
+                console.log('moviendo carta en posicion: '+ i);
+                // console.log('moviendo: '+deck[ i ].get('backgroundImage'));
+                collie.Timer.queue().
+                        transition( deck[ i ], 200, {
+                            to: deck[ i ].get( 'x' ) - ( widthCard / 2 ),
+                            effect: collie.Effect.easeOutBack, // sinusoidal easeIn, //collie.Effect.cubicBezier(0.16, 0.79, 0.92, 0.27),
+                            set: 'x'
+                        });
+            }
+        }
+    }
+
+
+}
+
+// dibuja las cartas del openente
 function drawDecksVsPlayers(){
-    var posX=10;
-    var posY=0;
-    var cardPosition=1;
-    var position=1;
-    var cardOffsetY = (heightDevice/2) - (heightCard*13);
-    console.log(cardOffsetY+(heightCard*(cardPosition+(position++))));
+    var posX = 10;
+    var posY = 0;
+    var cardPosition = 1;
+    var position = 1;
+    var cardOffsetY = ( heightDevice / 2 ) - 360;
+    console.log("heightDevice: "+heightDevice+", cardOffsetY: "+cardOffsetY);
+    // console.log(cardOffsetY+(heightCard*(cardPosition+(position++))));
     //cardOffsetY+(heightCard*(cardPosition+(position++)))
+
     // player left
-    for(var i = 0; i < 13 ; i++){
+    for(var i = 0; i < 13 ; i++){    
+        cardOffsetY+= 30;
+        // console.log("cardOffsetY: "+cardOffsetY);
         var tmpCard = new collie.DisplayObject({
-            x: posX, 
-            y: cardOffsetY+(heightCard*(cardPosition+(position++))),
-            scaleX:scale,
-            scaleY:scale,
-            originX: 'left',
-            originY: 'bottom',
-            useEvent : true,
+            x: 0, 
+            y: cardOffsetY,
+            scaleX: scale,
+            scaleY: scale,
+             originX: 'center',
+            // originY: 'left',
             backgroundImage: 'cBackHorizontal'
             // velocityRotate: 50
+        }).addTo( layer );
+    }
+
+    // player top
+    var noCards = 13;
+    var spotLength = ( heightDevice / 3 ) * 2;
+    posX=( widthDevice / 2 ) - ( spotLength / 2 );
+    var offset = spotLength / noCards;
+    
+    for(var i = 0; i < 13 ; i++){    
+        posX+= offset;
+        // console.log("cardOffsetY: "+cardOffsetY);
+        var tmpCard = new collie.DisplayObject({
+            x: posX, 
+            y: 10,
+            scaleX: scale,
+            scaleY: scale,
+            originX: 'left',
+            originY: 'top',
+            backgroundImage: 'cBack'
+            // velocityRotate: 50
         }).addTo(layer);
+    }
+
+    // player right
+    var cardOffsetY = ( heightDevice / 2 ) - 360;    
+    for(var i = 0; i < 13 ; i++){    
+        cardOffsetY+= 30;
+        // console.log("cardOffsetY: "+cardOffsetY);
+        var tmpCard = new collie.DisplayObject({
+            x: widthDevice - widthCard - 160, 
+            y: cardOffsetY,
+            scaleX:scale,
+            scaleY:scale,
+             originX: 'center',
+            // originY: 'left',
+            backgroundImage: 'cBackHorizontal'
+            // velocityRotate: 50
+        }).addTo( layer );
     }
 }
 
 
 
-
 // Attach animation for all layer
 layer.attach({
-
+     // mousemove : function (oEvent) {
+     //            console.log("moving...");
+     //            console.log(oEvent);
+     //            if (oEvent.displayObject) {
+     //                console.log("over card...");
+     //                oEvent.displayObject.set({
+     //                    scaleX : oEvent.displayObject.get("scale") * 1.2,
+     //                    scaleY : oEvent.displayObject.get("scale") * 1.2
+     //                });
+     //                oSelectedDisplayObject = oEvent.displayObject;
+     //                htStartPosition = {
+     //                    x : oEvent.x,
+     //                    y : oEvent.y 
+     //                };
+     //                htSelectedDisplayObjectPosition = {
+     //                    x : oSelectedDisplayObject.get("x"),
+     //                    y : oSelectedDisplayObject.get("y")
+     //                };
+     //            }
+     //        }
 });
 
 
-collie.Renderer.addLayer(layer);
-collie.Renderer.load(document.getElementById("container"));
+
+/* ----------------------------------------------------------------------------------------------------------------*/
+
+collie.Renderer.addLayer( layer );
+collie.Renderer.load( document.getElementById( 'container' ) );
 collie.Renderer.start();
 drawDeck();
 // drawDecksVsPlayers();
+/* ----------------------------------------------------------------------------------------------------------------*/
 
-
-
-
-
-
-window.onresize = function(event) {
+window.onresize = function( event ) {
   width = window.innerWidth
   height = window.innerHeight
 
@@ -278,8 +378,8 @@ window.onresize = function(event) {
   prevWidth = width;
   prevHeight = height;
 
-  canvas.setAttribute('width',width);  
-  canvas.setAttribute('height',height);
+  canvas.setAttribute( 'width', width );
+  canvas.setAttribute( 'height', height );
 
-  draw( );
+  // reDraw( );
 }
